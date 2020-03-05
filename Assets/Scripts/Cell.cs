e360420863e22e19ab2a964e4ab4bf8a5bb16dfd
ma- <mamorum@users.noreply.github.com>
2020-03-06 01:34:29 +0900

    Unityの資源を追加。

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+  internal int id;
+  public void Click() {
+    Debug.Log(id);
+  }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+  public Cell[] cells;
+  void Start() {
+    for (int i = 0; i < cells.Length; i++) {
+      cells[i].id = i;
+    }
+  }
+}
