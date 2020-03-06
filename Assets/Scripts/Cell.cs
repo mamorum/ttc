f3c52dae53f268369984c143bf6967e0aa2ed997
ma- <mamorum@users.noreply.github.com>
2020-03-06 16:04:21 +0900

    Clickイベントを実装。

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour {
+  public Image img; public Text txt;
+  public Color selected;
   internal int id;
-  public void Click() {
-    Debug.Log(id);
+  Controller c;
+  internal void Init(Controller c, int id) {
+    this.c = c; this.id = id;
+    img = GetComponent<Image>();
+    txt = GetComponentInChildren<Text>();
+  }
+  public void OnClick() {
+    c.OnClick(this);
+    c.ChangeSide();
   }
 }
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
   public Cell[] cells;
+  internal readonly int empty = 0, x = 1, o = 2;
+  int[] board; int turn;
   void Start() {
     for (int i = 0; i < cells.Length; i++) {
-      cells[i].id = i;
+      cells[i].Init(this, i);
     }
+    board = new int[cells.Length];
+    ResetGame();
+  }
+  void ResetGame() {
+    for (int i = 0; i < board.Length; i++) {
+      board[i] = empty;
+    }
+    turn = x;
+  }
+  internal void OnClick(Cell c) {
+    if (board[c.id] != empty) return;
+    board[c.id] = turn;
+    c.img.color = c.selected;
+    if (turn == x) c.txt.text = "X";
+    else c.txt.text = "O";
+  }
+  internal void ChangeSide() {
+    // TODO: 終了判定
+    if (turn == x) turn = o;
+    else turn = x;
   }
 }
